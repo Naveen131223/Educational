bcf56e2860bbf0ae9cfd2844c456e538b6e11f77
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-07-01 22:29:08 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,12 +1,8 @@
 import bot from './assets/bot.svg';
 import user from './assets/user.svg';
-import * as tf from '@tensorflow/tfjs';
-import * as tfn from '@tensorflow/tfjs-node';
-import fetch from 'node-fetch';
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
-const apiEndpoint = 'https://api.openai.com/v1/engines/davinci-codex/completions';
 
 let loadInterval;
 
@@ -98,15 +94,13 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   try {
-    const response = await fetch(apiEndpoint, {
+    const response = await fetch('https://educational-development.onrender.com/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer YOUR_API_KEY' // Replace with your OpenAI API key
       },
       body: JSON.stringify({
         prompt: prompt,
-        max_tokens: 50 // Adjust the max tokens as per your requirement
       }),
     });
 
@@ -115,7 +109,7 @@ const handleSubmit = async (e) => {
 
     if (response.ok) {
       const data = await response.json();
-      const parsedData = data.choices[0].text.trim(); // Trim any trailing spaces or '\n'
+      const parsedData = data.bot.trim(); // Trim any trailing spaces or '\n'
 
       // Display the bot's response with typing effect
       typeText(messageDiv, parsedData);
