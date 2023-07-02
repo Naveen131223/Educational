97da9295926fda00cc23ea7a9f2c0a224ad9e4a5
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-07-02 20:28:58 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -6,15 +6,31 @@ const chatContainer = document.querySelector('#chat_container');
 const input = form.querySelector('textarea');
 const submitButton = form.querySelector('button[type="submit"]');
 
-const generateUniqueId = () => {
+let loadInterval;
+
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    // Update the text content of the loading indicator
+    element.textContent += '.';
+
+    // If the loading indicator has reached three dots, reset it
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 100);
+}
+
+function generateUniqueId() {
   const timestamp = Date.now();
   const randomNumber = Math.random();
   const hexadecimalString = randomNumber.toString(16).slice(2, 8); // Generate a 6-digit hexadecimal string
 
   return `id-${timestamp}-${hexadecimalString}`;
-};
+}
 
-const createChatStripe = (isAi, value, uniqueId) => {
+function createChatStripe(isAi, value, uniqueId) {
   const profileImg = isAi ? bot : user;
 
   return `
@@ -29,79 +45,9 @@ const createChatStripe = (isAi, value, uniqueId) => {
       </div>
     </div>
   `;
-};
+}
 
-const loader = (element) => {
-  element.textContent = '';
-
-  const loadInterval = setInterval(() => {
-    // Update the text content of the loading indicator
-    element.textContent += '.';
-
-    // If the loading indicator has reached three dots, reset it
-    if (element.textContent === '....') {
-      element.textContent = '';
-    }
-  }, 100);
-  
-  return loadInterval;
-};
-
-const fetchBotResponse = async (prompt, uniqueId) => {
-  const messageDiv = document.getElementById(uniqueId);
-
-  try {
-    // Fetch the response from the server
-    const response = await fetch('https://educational-development.onrender.com/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        prompt: prompt,
-      }),
-    });
-
-    clearInterval(loadInterval);
-    messageDiv.textContent = '';
-
-    if (response.ok) {
-      const data = await response.json();
-      const parsedData = data.bot.trim(); // Trim any trailing spaces or '\n'
-
-      // Display the bot's response instantly
-      messageDiv.innerHTML = `
-        <span>${parsedData}</span>
-      `;
-
-      // Scroll to the latest message after rendering the response
-      scrollToLatestMessage();
-
-      // Re-enable the submit button after processing
-      submitButton.disabled = false;
-
-      // Focus on the input field for the next response
-      input.focus();
-
-      // Listen for user feedback on the response
-      listenForFeedback(prompt, parsedData);
-    } else {
-      const err = await response.text();
-
-      messageDiv.textContent = 'Something went wrong';
-      alert(err);
-
-      // Re-enable the submit button after processing
-      submitButton.disabled = false;
-    }
-  } catch (error) {
-    messageDiv.textContent = 'Something went wrong';
-    console.error(error);
-
-    // Re-enable the submit button after processing
-    submitButton.disabled = false;
-  }
-};
+let thinkingTimeout;
 
 const handleSubmit = async (e) => {
   e.preventDefault();
@@ -132,14 +78,65 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
 
   // Show the loading indicator
-  const loadInterval = loader(messageDiv);
+  loader(messageDiv);
 
-  // Simulate AI "thinking" with a shorter delay
-  setTimeout(() => {
-    fetchBotResponse(prompt, uniqueId);
-  }, 800); // Adjust the delay duration as needed
+  try {
+    // Simulate AI "thinking" with a shorter delay
+    thinkingTimeout = setTimeout(async () => {
+      // Fetch the response from the server
+      const response = await fetch('https://educational-development.onrender.com/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          prompt: prompt,
+        }),
+      });
+
+      clearInterval(loadInterval);
+      messageDiv.textContent = '';
+
+      if (response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim(); // Trim any trailing spaces or '\n'
+
+        // Display the bot's response instantly
+        messageDiv.innerHTML = `
+          <span>${parsedData}</span>
+        `;
+
+        // Scroll to the latest message after rendering the response
+        scrollToLatestMessage();
+
+        // Re-enable the submit button after processing
+        submitButton.disabled = false;
+
+        // Focus on the input field for the next response
+        input.focus();
+
+        // Listen for user feedback on the response
+        listenForFeedback(prompt, parsedData);
+      } else {
+        const err = await response.text();
+
+        messageDiv.textContent = 'Something went wrong';
+        alert(err);
+
+        // Re-enable the submit button after processing
+        submitButton.disabled = false;
+      }
+    }, 800); // Adjust the delay duration as needed
+  } catch (error) {
+    messageDiv.textContent = 'Something went wrong';
+    console.error(error);
+
+    // Re-enable the submit button after processing
+    submitButton.disabled = false;
+  }
 };
 
+// Function to listen for user feedback on the AI response
 const listenForFeedback = (prompt, botResponse) => {
   const feedbackForm = document.createElement('form');
   const feedbackInput = document.createElement('input');
@@ -186,6 +183,7 @@ const listenForFeedback = (prompt, botResponse) => {
   });
 };
 
+// Function to send feedback to the server for model improvement
 const sendFeedback = async (prompt, botResponse, feedback) => {
   try {
     const response = await fetch('https://educational-development.onrender.com/feedback', {
@@ -208,28 +206,22 @@ const sendFeedback = async (prompt, botResponse, feedback) => {
   }
 };
 
-const scrollToLatestMessage = () => {
-  chatContainer.scrollTo({
-    top: chatContainer.scrollHeight,
-    behavior: 'smooth',
-  });
-};
-
-const handleFormSubmit = (e) => {
-  e.preventDefault();
-  handleSubmit();
-};
-
-form.addEventListener('submit', handleFormSubmit);
+form.addEventListener('submit', handleSubmit);
 form.addEventListener('keyup', (e) => {
   if (e.keyCode === 13) {
-    handleFormSubmit(e);
+    handleSubmit(e);
   }
 });
 
 // Auto-scroll to the latest message smoothly
-const handleWindowLoad = () => {
-  scrollToLatestMessage();
-};
+function scrollToLatestMessage() {
+  chatContainer.scrollTo({
+    top: chatContainer.scrollHeight,
+    behavior: 'smooth',
+  });
+}
 
-window.addEventListener('load', handleWindowLoad);
+// Scroll to the latest message on initial load
+window.addEventListener('load', () => {
+  scrollToLatestMessage();
+});
