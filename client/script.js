3dc2f121b3b977cdaca95551e49f31d06e68bdd4
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-07-02 17:02:01 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -39,7 +39,10 @@ function createChatStripe(isAi, value, uniqueId) {
         <div class="profile">
           <img src="${profileImg}" alt="${isAi ? 'bot' : 'user'}" />
         </div>
-        <div class="message" id="${uniqueId}">${value}</div>
+        <div class="message" id="${uniqueId}">
+          <span>${value}</span>
+          ${isAi ? '<button class="copy-btn">Copy</button>' : ''}
+        </div>
       </div>
     </div>
   `;
@@ -100,7 +103,10 @@ const handleSubmit = async (e) => {
         const parsedData = data.bot.trim(); // Trim any trailing spaces or '\n'
 
         // Display the bot's response instantly
-        messageDiv.textContent = parsedData;
+        messageDiv.innerHTML = `
+          <span>${parsedData}</span>
+          <button class="copy-btn">Copy</button>
+        `;
 
         // Scroll to the latest message after rendering the response
         scrollToLatestMessage();
@@ -209,4 +215,29 @@ function scrollToLatestMessage() {
 }
 
 // Scroll to the latest message on initial load
-window.addEventListener('load', scrollToLatestMessage);
+window.addEventListener('load', () => {
+  scrollToLatestMessage();
+
+  // Copy button event listeners
+  const copyButtons = document.querySelectorAll('.copy-btn');
+  copyButtons.forEach((button) => {
+    button.addEventListener('click', handleCopy);
+  });
+});
+
+// Copy button event handler
+function handleCopy(event) {
+  const message = event.target.previousElementSibling.textContent;
+  copyToClipboard(message);
+  alert('Copied to clipboard!');
+}
+
+// Function to copy text to clipboard
+function copyToClipboard(text) {
+  const tempInput = document.createElement('input');
+  tempInput.value = text;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand('copy');
+  document.body.removeChild(tempInput);
+}
