26cf5a77a75d1eb4619e7a8e88a1e876d7c55949
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-07-06 14:40:57 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -139,7 +139,7 @@ const handleSubmit = async (e) => {
         // Re-enable the submit button after processing
         submitButton.disabled = false;
       }
-    }, 80); // Adjust the delay duration as needed
+    }, 60); // Adjust the AI delay duration as needed
   } catch (error) {
     messageDiv.textContent = 'Something went wrong';
     console.error(error);
