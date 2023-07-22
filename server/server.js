7430dbc4f27901a7c55f678c29bbccf20fd1c3d1
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-07-22 17:57:56 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,11 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
-
-dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const model = process.env.OPENAI_MODEL || 'text-davinci-003';
+const maxTokens = 3000;
 
-app.use(cors());
 app.use(express.json());
 
 const apiKey = process.env.OPENAI_API_KEY;
@@ -18,42 +15,38 @@ if (!apiKey) {
   process.exit(1);
 }
 
-const configuration = new Configuration({
-  apiKey,
-});
-
+const configuration = new Configuration({ apiKey });
 const openai = new OpenAIApi(configuration);
 
-// Simple in-memory cache to store AI responses
+// Simple in-memory cache to store AI responses' choices
 const responseCache = {};
 
 async function preloadModel() {
   try {
     const prompt = 'Preloading AI model...'; // A placeholder prompt for preloading
-    const aiResponse = await openai.createCompletion({
-      model: process.env.OPENAI_MODEL || 'text-davinci-003',
-      prompt: `${prompt}`,
-      temperature: 0,
-      max_tokens: 1,
-    });
-
+    await openai.createCompletion({ model, prompt, temperature: 0, max_tokens: 1 });
     console.log('AI model preloaded successfully!');
   } catch (error) {
     console.error('Failed to preload AI model:', error);
+    process.exit(1);
   }
 }
 
 preloadModel(); // Preload the AI model asynchronously during server startup
 
+function validatePrompt(prompt) {
+  return typeof prompt === 'string' && prompt.trim() !== '';
+}
+
 app.post('/', async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    if (!prompt || typeof prompt !== 'string' || prompt.trim() === '') {
+    if (!validatePrompt(prompt)) {
       return res.status(400).send({ error: 'Invalid or missing prompt in the request body.' });
     }
 
-    // Check if the response is cached
+    // Check if the response choices are cached
     if (responseCache[prompt]) {
       console.log('Cache hit for prompt:', prompt);
       return res.status(200).send({ bot: responseCache[prompt] });
@@ -61,17 +54,17 @@ app.post('/', async (req, res) => {
 
     // Send the request to the AI model asynchronously
     const aiResponse = await openai.createCompletion({
-      model: process.env.OPENAI_MODEL || 'text-davinci-003',
+      model,
       prompt: `${prompt}`,
       temperature: 0,
-      max_tokens: 3000,
+      max_tokens: maxTokens,
       top_p: 1,
       frequency_penalty: 0.5,
       presence_penalty: 0,
     });
 
-    const botResponse = aiResponse.data.choices[0]?.text || 'No response from the AI model.';
-    responseCache[prompt] = botResponse;
+    const botResponse = aiResponse.data.choices?.[0]?.text || 'No response from the AI model.';
+    responseCache[prompt] = aiResponse.data.choices; // Cache the model's choices array
 
     res.status(200).send({ bot: botResponse });
   } catch (error) {
