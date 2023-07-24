e13eb8f0c975c22cedf2bc35cb883fbace3ab81e
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-07-24 20:09:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,184 +1,48 @@
-import express from 'express';
-import cors from 'cors';
-import compression from 'compression';
-import { Configuration, OpenAIApi } from 'openai';
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
 
-const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(cors());
-app.use(express.json({ limit: '1mb' }));
-app.use(compression());
-
-const apiKey = process.env.OPENAI_API_KEY;
-
-if (!apiKey) {
-  console.error('Please provide an OPENAI_API_KEY in your environment variables.');
-  process.exit(1);
-}
+dotenv.config()
 
 const configuration = new Configuration({
-  apiKey,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
-// Simple in-memory cache to store API responses
-const responseCache = new Map();
-const MAX_CACHE_SIZE = 1000; // Limit the number of cached responses
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-let isAIModelReady = false; // Flag to check if the AI model is ready
-const WARM_UP_PROMPT = 'Warm-up prompt';
-
-// Initialize the AI model asynchronously during server startup
-const modelInitializationPromise = initializeAIModel();
-
-async function initializeAIModel() {
-  try {
-    console.log('Initializing AI model...');
-    const response = await openai.createCompletion({
-      model: process.env.OPENAI_MODEL || 'text-davinci-003',
-      prompt: WARM_UP_PROMPT,
-      max_tokens: 1, // Only requesting one token to reduce unnecessary response data
-    });
-
-    const botResponse = response.data.choices[0]?.text || 'No response from the AI model.';
-    if (botResponse.toLowerCase() === 'ok') {
-      // Check if the AI model's response is satisfactory for warm-up prompt
-      console.log('AI model is ready!');
-      isAIModelReady = true;
-    } else {
-      console.error('AI model warm-up response is not satisfactory:', botResponse);
-    }
-  } catch (error) {
-    console.error('Error initializing AI model:', error);
-    // You may want to retry the initialization in case of failure
-  }
-}
-
-app.use((req, res, next) => {
-  // If the AI model is not ready and the request is not for the initial warm-up prompt,
-  // send a message indicating that the AI model is initializing.
-  if (!isAIModelReady && req.body.prompt !== WARM_UP_PROMPT) {
-    res.status(200).send({
-      message: 'Initializing AI model, please wait...',
-    });
-  } else {
-    next();
-  }
-});
-
-// Endpoint to provide information on the AI model's initialization status
-app.get('/status', (req, res) => {
-  if (isAIModelReady) {
-    res.status(200).send({
-      status: 'AI model is ready!',
-    });
-  } else {
-    res.status(200).send({
-      status: 'AI model is initializing...',
-    });
-  }
-});
-
-app.get('/', (req, res) => {
-  if (isAIModelReady) {
-    // If the AI model is ready, return the cached warm-up response immediately
-    res.status(200).send({
-      bot: responseCache.get(WARM_UP_PROMPT),
-    });
-  } else {
-    // If the AI model is still initializing, send a message indicating the same
-    res.status(200).send({
-      message: 'Initializing AI model, please wait...',
-    });
-  }
-});
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
+})
 
 app.post('/', async (req, res) => {
   try {
-    let { prompt } = req.body;
+    const prompt = req.body.prompt;
 
-    if (!prompt || typeof prompt !== 'string' || prompt.trim() === '') {
-      return res.status(400).send({ error: 'Invalid or missing prompt in the request body.' });
-    }
-
-    // Sanitize and escape the input prompt to prevent XSS attacks
-    prompt = sanitizeInput(prompt);
-
-    // Check if the response is cached
-    if (responseCache.has(prompt)) {
-      console.log('Cache hit for prompt:', prompt);
-      return res.status(200).send({ bot: responseCache.get(prompt) });
-    }
-
-    // Send the request to the AI model asynchronously
     const response = await openai.createCompletion({
-      model: process.env.OPENAI_MODEL || 'text-davinci-003',
+      model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0.7, // Higher temperature can make the output more diverse
-      max_tokens: 200, // Limit the response length to 200 tokens for faster output
-      top_p: 0.8, // Adjust the top-p parameter to control the response randomness
-      frequency_penalty: 0.2,
-      presence_penalty: 0.2,
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
-    const botResponse = response.data.choices[0]?.text || 'No response from the AI model.';
-    responseCache.set(prompt, botResponse);
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
 
-    // Limit the cache size by removing the least recently used entry
-    if (responseCache.size > MAX_CACHE_SIZE) {
-      const leastRecentlyUsedKey = responseCache.keys().next().value;
-      responseCache.delete(leastRecentlyUsedKey);
-    }
-
-    res.status(200).send({ bot: botResponse });
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Something went wrong');
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
   }
-});
+})
 
-// Error handler middleware
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send('Something went wrong');
-});
-
-// Start the server after the AI model is initialized
-modelInitializationPromise.then(() => {
-  const server = app.listen(port, () => {
-    console.log(`AI server started on http://localhost:${port}`);
-  });
-
-  process.on('SIGTERM', () => {
-    console.log('Shutting down gracefully...');
-    server.close(() => {
-      console.log('Server has been closed.');
-      process.exit(0);
-    });
-  });
-});
-
-function sanitizeInput(input) {
-  // Function to sanitize and escape input to prevent XSS attacks
-  // Replace any potentially dangerous characters with their HTML entity representation
-  return input.replace(/[&<>"'\/]/g, (char) => {
-    switch (char) {
-      case '&':
-        return '&amp;';
-      case '<':
-        return '&lt;';
-      case '>':
-        return '&gt;';
-      case '"':
-        return '&quot;';
-      case "'":
-        return '&#39;';
-      case '/':
-        return '&#x2F;';
-      default:
-        return char;
-    }
-  });
-}
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
