559ccd3ad93ca38d1d488e6462195c020b1dac38
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-07-25 09:12:15 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import helmet from 'helmet'; // Added Helmet for security headers
+import rateLimit from 'express-rate-limit'; // Added rate limiting
 import { Configuration, OpenAIApi } from 'openai';
-import cluster from 'cluster';
-import os from 'os';
 
 dotenv.config();
 
@@ -13,67 +13,66 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const numCPUs = os.cpus().length;
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
+// Use Helmet middleware to set appropriate security headers
+app.use(helmet());
 
-  // Fork workers for each CPU core
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+// Implement rate limiting to prevent abuse
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
+// Store the last response to be reused if the same prompt is requested again.
+let lastResponse = null;
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!',
   });
-} else {
-  const app = express();
-  
-  // Set up CORS configuration
-  const allowedOrigins = ['https://example.com', 'https://yourdomain.com'];
-  app.use(cors({
-    origin: function (origin, callback) {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  }));
-  
-  app.use(express.json());
+});
 
-  app.get('/', async (req, res) => {
-    res.status(200).send({
-      message: 'Hello from CodeX!',
-    });
-  });
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
 
-  app.post('/', async (req, res) => {
-    try {
-      // Validate and sanitize input
-      const prompt = req.body.prompt;
-
-      // Implement rate limiting here
-      
-      const response = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: `${prompt}`,
-        temperature: 0,
-        max_tokens: 3000,
-        top_p: 1,
-        frequency_penalty: 0.5,
-        presence_penalty: 0,
+    // Validate and sanitize the input prompt
+    if (!prompt || typeof prompt !== 'string') {
+      return res.status(400).send({
+        error: 'Invalid prompt',
       });
-
-      res.status(200).send({
-        bot: response.data.choices[0].text,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Something went wrong');
     }
-  });
 
-  app.listen(5000, () => console.log(`AI server started on http://localhost:5000 (Worker ${process.pid})`));
-}
+    // Check if the same prompt was requested recently, and reuse the response.
+    if (lastResponse && lastResponse.prompt === prompt) {
+      return res.status(200).send({
+        bot: lastResponse.bot,
+      });
+    }
+
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${prompt}`,
+      temperature: 0.7, // Using a slightly higher temperature for more creative responses.
+      max_tokens: 150, // Limiting the response length to improve speed.
+    });
+
+    const botResponse = response.data.choices[0].text;
+
+    // Cache the current response for reuse.
+    lastResponse = { prompt, bot: botResponse };
+
+    res.status(200).send({
+      bot: botResponse,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Something went wrong');
+  }
+});
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
