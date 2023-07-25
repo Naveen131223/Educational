48453f70875ee3b5923b61634065af202dc48037
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-07-25 09:18:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import helmet from 'helmet'; // Added Helmet for security headers
-import rateLimit from 'express-rate-limit'; // Added rate limiting
 import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
@@ -17,45 +15,42 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Use Helmet middleware to set appropriate security headers
-app.use(helmet());
-
-// Implement rate limiting to prevent abuse
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
-});
-app.use(limiter);
-
 // Store the last response to be reused if the same prompt is requested again.
 let lastResponse = null;
 
-app.get('/', async (req, res) => {
-  res.status(200).send({
-    message: 'Hello from CodeX!',
-  });
-});
+// Sample rate limiting implementation (Adjust as per your requirements)
+const RATE_LIMIT_WINDOW_MS = 60 * 1000; // 1 minute
+const MAX_REQUESTS_PER_WINDOW = 5;
+const rateLimitMap = new Map();
 
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    // Validate and sanitize the input prompt
-    if (!prompt || typeof prompt !== 'string') {
-      return res.status(400).send({
-        error: 'Invalid prompt',
-      });
+    // Rate Limiting Check
+    const clientIp = req.ip; // Requires trust proxy setting
+    const currentTime = Date.now();
+    let requestCount = rateLimitMap.get(clientIp) || 0;
+
+    if (requestCount >= MAX_REQUESTS_PER_WINDOW) {
+      return res.status(429).send('Too many requests. Please try again later.');
     }
+
+    rateLimitMap.set(clientIp, requestCount + 1);
+
+    setTimeout(() => {
+      rateLimitMap.set(clientIp, (rateLimitMap.get(clientIp) || 1) - 1);
+    }, RATE_LIMIT_WINDOW_MS);
 
     // Check if the same prompt was requested recently, and reuse the response.
     if (lastResponse && lastResponse.prompt === prompt) {
       return res.status(200).send({
-        bot: lastResponse.bot,
+        bot: lastResponse.bot
       });
     }
 
     const response = await openai.createCompletion({
-      model: 'text-davinci-003',
+      model: "text-davinci-003",
       prompt: `${prompt}`,
       temperature: 0.7, // Using a slightly higher temperature for more creative responses.
       max_tokens: 150, // Limiting the response length to improve speed.
@@ -67,7 +62,7 @@ app.post('/', async (req, res) => {
     lastResponse = { prompt, bot: botResponse };
 
     res.status(200).send({
-      bot: botResponse,
+      bot: botResponse
     });
   } catch (error) {
     console.error(error);
