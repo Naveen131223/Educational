2d7ebe5d4552f8dd834ba26271cbf72108ef34f0
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-07-25 09:22:04 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,29 +18,15 @@ app.use(express.json());
 // Store the last response to be reused if the same prompt is requested again.
 let lastResponse = null;
 
-// Sample rate limiting implementation (Adjust as per your requirements)
-const RATE_LIMIT_WINDOW_MS = 60 * 1000; // 1 minute
-const MAX_REQUESTS_PER_WINDOW = 5;
-const rateLimitMap = new Map();
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hi Sister'
+  });
+});
 
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
-
-    // Rate Limiting Check
-    const clientIp = req.ip; // Requires trust proxy setting
-    const currentTime = Date.now();
-    let requestCount = rateLimitMap.get(clientIp) || 0;
-
-    if (requestCount >= MAX_REQUESTS_PER_WINDOW) {
-      return res.status(429).send('Too many requests. Please try again later.');
-    }
-
-    rateLimitMap.set(clientIp, requestCount + 1);
-
-    setTimeout(() => {
-      rateLimitMap.set(clientIp, (rateLimitMap.get(clientIp) || 1) - 1);
-    }, RATE_LIMIT_WINDOW_MS);
 
     // Check if the same prompt was requested recently, and reuse the response.
     if (lastResponse && lastResponse.prompt === prompt) {
@@ -52,8 +38,8 @@ app.post('/', async (req, res) => {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0.7, // Using a slightly higher temperature for more creative responses.
-      max_tokens: 150, // Limiting the response length to improve speed.
+      temperature: 0.8, // Using a slightly higher temperature for more creative responses.
+      max_tokens: 15000, // Limiting the response length to improve speed.
     });
 
     const botResponse = response.data.choices[0].text;
@@ -66,7 +52,7 @@ app.post('/', async (req, res) => {
     });
   } catch (error) {
     console.error(error);
-    res.status(500).send('Something went wrong');
+    res.status(500).send(error || 'Something went wrong');
   }
 });
 
