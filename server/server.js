6e23782506d747be0eb3c3816c4700b5ee5afd79
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-07-25 10:15:19 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+import rateLimit from 'express-rate-limit';
+import sanitizeHtml from 'sanitize-html';
 
-dotenv.config()
+dotenv.config();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -11,38 +13,57 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Function to sanitize user input
+const sanitizeUserInput = (input) => {
+  const sanitizedInput = sanitizeHtml(input, {
+    allowedTags: [],
+    allowedAttributes: {},
+  });
+  return sanitizedInput;
+};
+
+// Rate limiting configuration
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 100, // 100 requests per minute
+});
+
+app.use(limiter); // Apply rate limiting to all routes
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hi Sister'
-  })
-})
+    message: 'Hi Sister',
+  });
+});
 
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
+    // Sanitize user input before using it
+    const sanitizedPrompt = sanitizeUserInput(prompt);
+
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      model: 'text-davinci-003',
+      prompt: `${sanitizedPrompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
     });
 
     res.status(200).send({
-      bot: response.data.choices[0].text
+      bot: response.data.choices[0].text,
     });
-
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res.status(500).send(error || 'Something went wrong');
   }
-})
+});
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
