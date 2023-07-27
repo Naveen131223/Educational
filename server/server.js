33af8e5a1ede57aab7c7fb04d337ecc6eb1d29a4
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-07-27 14:46:51 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,30 +21,42 @@ function sanitizeInput(input) {
   return sanitizedInput;
 }
 
-app.get('/', async (req, res) => {
-  res.status(200).send({
-    message: 'Hi Sister',
-  });
-});
+// In-memory cache to store previous API responses
+const responseCache = {};
+
+// Function to check if a prompt is already cached
+function isCached(prompt) {
+  return responseCache.hasOwnProperty(prompt);
+}
 
 app.post('/', async (req, res) => {
   try {
     const prompt = sanitizeInput(req.body.prompt);
 
-    // Perform the API call asynchronously using async/await
-    const response = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: `${prompt}`,
-      temperature: 0.2,
-      max_tokens: 3000,
-      top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0,
-    });
+    // Check if the response is already cached
+    if (isCached(prompt)) {
+      res.status(200).send({
+        bot: responseCache[prompt],
+      });
+    } else {
+      // Perform the API call asynchronously using async/await
+      const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `${prompt}`,
+        temperature: 0.2,
+        max_tokens: 3000,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0,
+      });
 
-    res.status(200).send({
-      bot: response.data.choices[0].text,
-    });
+      // Cache the response for future use
+      responseCache[prompt] = response.data.choices[0].text;
+
+      res.status(200).send({
+        bot: response.data.choices[0].text,
+      });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send('Something went wrong');
