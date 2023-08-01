8b5f29b7fef3db477f3a59106ecd1cda3f117a90
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-08-01 17:42:13 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+import rateLimit from 'express-rate-limit';
 
-dotenv.config()
+dotenv.config();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -11,38 +12,54 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Create a rate limiter to limit the number of requests
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 10, // 10 requests per minute
+});
+
+app.use(limiter);
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodeX!'
-  })
-})
+    message: 'Hello from CodeX!',
+  });
+});
 
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
+    if (!prompt || typeof prompt !== 'string') {
+      return res.status(400).send({ error: 'Invalid request. "prompt" must be a non-empty string.' });
+    }
+
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: 'text-davinci-003',
       prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
     });
 
-    res.status(200).send({
-      bot: response.data.choices[0].text
-    });
-
+    if (response.data && response.data.choices && response.data.choices[0] && response.data.choices[0].text) {
+      return res.status(200).send({
+        bot: response.data.choices[0].text,
+      });
+    } else {
+      return res.status(500).send({ error: 'Something went wrong with the AI response.' });
+    }
   } catch (error) {
-    console.error(error)
-    res.status(500).send(error || 'Something went wrong');
+    console.error(error);
+    return res.status(500).send({ error: error.message || 'Something went wrong' });
   }
-})
+});
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`AI server started on http://localhost:${port}`));
