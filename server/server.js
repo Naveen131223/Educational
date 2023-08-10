32cd043c4ebcb271bbcd2ec240805c530478043b
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-08-10 21:03:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,9 +7,13 @@ app.use(express.json({ limit: '1mb' })); // Limit request size to 1MB
 
 app.post('/', async (req, res) => {
   try {
-    // You can simply send a fixed response here
+    const userMessage = req.body.message; // Assuming the client sends a 'message' field
+
+    // Generate a response based on the user's input
+    const botResponse = generateBotResponse(userMessage);
+
     res.status(200).send({
-      bot: "How can I help you?",
+      bot: botResponse,
     });
   } catch (error) {
     console.error(error);
@@ -17,6 +21,21 @@ app.post('/', async (req, res) => {
   }
 });
 
-const PORT = 5000; 
+function generateBotResponse(inputMessage) {
+  // Here you can implement a logic to generate responses based on user input.
+  // For demonstration purposes, using predefined responses.
+  const responses = [
+    "How can I assist you?",
+    "Is there anything else you'd like to know?",
+    "Feel free to ask any questions.",
+    "I'm here to help. What can I do for you?",
+  ];
+
+  // Select a response randomly
+  const randomIndex = Math.floor(Math.random() * responses.length);
+  return responses[randomIndex];
+}
+
+const PORT = 5000;
 
 app.listen(PORT, () => console.log(`AI server started on http://localhost:${PORT}`));
