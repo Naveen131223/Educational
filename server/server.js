48d178c5d7c051d8efe9ff90bbe1ca86bd5acf91
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-08-20 21:30:52 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,14 +3,13 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '1mb' })); // Limit request size to 1MB
+app.use(express.json({ limit: '1mb' }));
 
 app.post('/', async (req, res) => {
   try {
-    const userMessage = req.body.message; // Assuming the client sends a 'message' field
+    const userMessage = req.body.message;
 
-    // Generate a response based on the user's input
-    const botResponse = generateBotResponse(userMessage);
+    const botResponse = await generateBotResponse(userMessage);
 
     res.status(200).send({
       bot: botResponse,
@@ -21,20 +20,20 @@ app.post('/', async (req, res) => {
   }
 });
 
-function generateBotResponse(inputMessage) {
-  // Here you can implement a logic to generate responses based on user input.
-  // For demonstration purposes, using predefined responses.
-  const responses = [
-    "How can I assist you?",
-    "How can I help you?",
-    "Is there anything else you'd like to know?",
-    "Feel free to ask any questions.",
-    "I'm here to help. What can I do for you?",
-  ];
+async function generateBotResponse(inputMessage) {
+  // Normalize user input for case-insensitive comparison
+  const normalizedInput = inputMessage.toLowerCase();
 
-  // Select a response randomly
-  const randomIndex = Math.floor(Math.random() * responses.length);
-  return responses[randomIndex];
+  // Check for specific input messages and provide corresponding responses
+  if (normalizedInput.includes('hi sister')) {
+    return "How can I help you?";
+  } else if (normalizedInput.includes('hello')) {
+    return "Hello! How can I assist you?";
+  } else if (normalizedInput.includes('bye')) {
+    return "Goodbye! Feel free to ask more questions later.";
+  } else {
+    return "I'm here to help. What can I do for you?";
+  }
 }
 
 const PORT = 5000;
