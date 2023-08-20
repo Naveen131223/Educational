96b5b5287a61e087b14c4816d26da2f01c4d6da6
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-08-20 19:52:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,42 +1,48 @@
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
 
-const app = express();
-app.use(cors());
-app.use(express.json({ limit: '1mb' })); // Limit request size to 1MB
+dotenv.config()
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
+})
 
 app.post('/', async (req, res) => {
   try {
-    const userMessage = req.body.message; // Assuming the client sends a 'message' field
+    const prompt = req.body.prompt;
 
-    // Generate a response based on the user's input
-    const botResponse = generateBotResponse(userMessage);
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    });
 
     res.status(200).send({
-      bot: botResponse,
+      bot: response.data.choices[0].text
     });
+
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Something went wrong');
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
   }
-});
+})
 
-function generateBotResponse(inputMessage) {
-  // Here you can implement a logic to generate responses based on user input.
-  // For demonstration purposes, using predefined responses.
-  const responses = [
-    "How can I assist you?",
-    "How can I help you?",
-    "Is there anything else you'd like to know?",
-    "Feel free to ask any questions.",
-    "I'm here to help. What can I do for you?",
-  ];
-
-  // Select a response randomly
-  const randomIndex = Math.floor(Math.random() * responses.length);
-  return responses[randomIndex];
-}
-
-const PORT = 5000;
-
-app.listen(PORT, () => console.log(`AI server started on http://localhost:${PORT}`));
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
