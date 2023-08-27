a587da8be7abdc7800971e7f5f82594cd8567c2e
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-08-27 22:21:57 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,42 +1,40 @@
 import express from 'express';
 import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '1mb' })); // Limit request size to 1MB
+app.use(express.json());
 
-app.post('/', async (req, res) => {
-  try {
-    const userMessage = req.body.message; // Assuming the client sends a 'message' field
-
-    // Generate a response based on the user's input
-    const botResponse = generateBotResponse(userMessage);
-
-    res.status(200).send({
-      bot: botResponse,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Something went wrong');
-  }
+app.get('/', async (req, res) => {
+res.status(200).send({
+message: 'Hello from CodeX!'
+});
 });
 
-function generateBotResponse(inputMessage) {
-  // Here you can implement a logic to generate responses based on user input.
-  // For demonstration purposes, using predefined responses.
-  const responses = [
-    "How can I assist you?",
-    "How can I help you?",
-    "Is there anything else you'd like to know?",
-    "Feel free to ask any questions.",
-    "I'm here to help. What can I do for you?",
-  ];
-
-  // Select a response randomly
-  const randomIndex = Math.floor(Math.random() * responses.length);
-  return responses[randomIndex];
+app.post('/', async (req, res) => {
+try {
+const prompt = req.body.prompt.text;
+const apiKey = process.env.GOOGLE_API_KEY; // Access the environment variable
+if (!apiKey) {
+throw new Error('Google API key not found in environment variable');
 }
+const apiUrl = `https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=${apiKey}`;
+const response = await axios.post(apiUrl, {
+prompt: prompt,
+max_tokens: 3000,
+top_p: 1,
+frequency_penalty: 0.5,
+presence_penalty: 0
+});
+const botResponse = response.data.text;
+res.status(200).send({
+bot: botResponse
+});
+} catch (error) {
+console.error(error);
+res.status(500).send(error || 'Something went wrong');
+}
+});
 
-const PORT = 5000;
-
-app.listen(PORT, () => console.log(`AI server started on http://localhost:${PORT}`));
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
