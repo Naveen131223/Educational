6727329b0c5937ac5d9b7c54d9eedf6048102bad
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-08-28 11:53:55 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-import google.generativeai as plam;
 import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
@@ -8,34 +7,34 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-res.status(200).send({
-message: 'Hello from CodeX!'
-});
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  });
 });
 
 app.post('/', async (req, res) => {
-try {
-const prompt = req.body.prompt.text;
-const apiKey = process.env.GOOGLE_API_KEY; // Access the environment variable
-if (!apiKey) {
-throw new Error('Google API key not found in environment variable');
-}
-const apiUrl = `https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=${apiKey}`;
-const response = await axios.post(apiUrl, {
-prompt: prompt,
-max_tokens: 3000,
-top_p: 1,
-frequency_penalty: 0.5,
-presence_penalty: 0
-});
-const botResponse = response.data.text;
-res.status(200).send({
-bot: botResponse
-});
-} catch (error) {
-console.error(error);
-res.status(500).send(error || 'Something went wrong');
-}
+  try {
+    const prompt = req.body.prompt; // Corrected line
+    const apiKey = process.env.GOOGLE_API_KEY; // Access the environment variable
+    if (!apiKey) {
+      throw new Error('Google API key not found in environment variable');
+    }
+    const apiUrl = `https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=${apiKey}`;
+    const response = await axios.post(apiUrl, {
+      prompt: prompt,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0
+    });
+    const botResponse = response.data.text;
+    res.status(200).send({
+      bot: botResponse
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error || 'Something went wrong');
+  }
 });
 
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
