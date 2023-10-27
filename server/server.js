011e97a98a310a484cfd7939699f6869b9611fec
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-10-27 21:18:43 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,35 +6,41 @@ app.use(cors());
 app.use(express.json({ limit: '1mb' })); // Limit request size to 1MB
 
 app.post('/', async (req, res) => {
-try {
-const userMessage = req.body.message; // Assuming the client sends a 'message' field
+  try {
+    const userMessage = req.body.message; // Assuming the client sends a 'message' field
 
-// Generate a response based on the user's input
-const botResponse = generateBotResponse(userMessage);
+    // Generate a response based on the user's input
+    const botResponse = generateBotResponse(userMessage);
 
-res.status(200).send({
-bot: botResponse,
-});
-} catch (error) {
-console.error(error);
-res.status(500).send('Something went wrong');
-}
+    res.status(200).send({
+      bot: botResponse,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Something went wrong');
+  }
 });
 
 function generateBotResponse(inputMessage) {
-// Here you can implement a logic to generate responses based on user input.
-// For demonstration purposes, using predefined responses.
-const responses = [
-"How can I assist you?",
-"How can I help you?",
-"Is there anything else you'd like to know?",
-"Feel free to ask any questions.",
-"I'm here to help. What can I do for you?",
-];
+  const lowerCaseMessage = inputMessage.toLowerCase();
 
-// Select a response randomly
-const randomIndex = Math.floor(Math.random() * responses.length);
-return responses[randomIndex];
+  if (lowerCaseMessage.includes('hi sister')) {
+    return "Hi there! How can I help you?";
+  } else if (lowerCaseMessage.includes('i am fine')) {
+    return "I'm glad to know you're fine!";
+  } else {
+    // For any other messages, provide a generic response
+    const genericResponses = [
+      "How can I assist you?",
+      "Is there anything else you'd like to know?",
+      "Feel free to ask any questions.",
+      "I'm here to help. What can I do for you?",
+    ];
+
+    // Select a response randomly
+    const randomIndex = Math.floor(Math.random() * genericResponses.length);
+    return genericResponses[randomIndex];
+  }
 }
 
 const PORT = 5000;
