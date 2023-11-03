50ad4ad64b4c85ce7995a0b1d0cd876a9c891b5c
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-11-03 21:43:10 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,42 +1,75 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
 import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+import readlineSync from 'readline-sync';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '1mb' })); // Limit request size to 1MB
+app.use(express.json());
+
+const apiKey = process.env.OPENAI_API_KEY || null; // Set apiKey to null initially
+
+let openai;
+if (apiKey) {
+  const configuration = new Configuration({
+    apiKey,
+  });
+  openai = new OpenAIApi(configuration);
+}
+
+app.get('/', async (req, res) => {
+  const defaultResponses = [
+    "How can I assist you?",
+    "How can I help you?",
+    "Is there anything else you'd like to know?",
+    "Feel free to ask any questions.",
+    "I'm here to help. What can I do for you?",
+  ];
+
+  const randomIndex = Math.floor(Math.random() * defaultResponses.length);
+  const defaultMessage = defaultResponses[randomIndex];
+
+  res.status(200).send({
+    message: 'Hello from CodeX!',
+    defaultResponse: defaultMessage,
+  });
+});
 
 app.post('/', async (req, res) => {
-try {
-const userMessage = req.body.message; // Assuming the client sends a 'message' field
+  try {
+    if (!openai) {
+      return res.status(403).send({
+        error: 'OpenAI API key not provided.',
+      });
+    }
 
-// Generate a response based on the user's input
-const botResponse = generateBotResponse(userMessage);
+    const prompt = req.body.prompt;
 
-res.status(200).send({
-bot: botResponse,
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error || 'Something went wrong');
+  }
 });
-} catch (error) {
-console.error(error);
-res.status(500).send('Something went wrong');
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
+
+function askUserForApiKey() {
+  const apiKey = readlineSync.question('Enter your OpenAI API key: ');
+  return apiKey;
 }
-});
-
-function generateBotResponse(inputMessage) {
-// Here you can implement a logic to generate responses based on user input.
-// For demonstration purposes, using predefined responses.
-const responses = [
-"How can I assist you?",
-"How can I help you?",
-"Is there anything else you'd like to know?",
-"Feel free to ask any questions.",
-"I'm here to help. What can I do for you?",
-];
-
-// Select a response randomly
-const randomIndex = Math.floor(Math.random() * responses.length);
-return responses[randomIndex];
-}
-
-const PORT = 5000;
-
-app.listen(PORT, () => console.log(`AI server started on http://localhost:${PORT}`));
