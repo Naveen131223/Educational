75be42de3e4ce535594a095d0659f6e23c9fd1e8
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-11-04 06:54:06 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,75 +1,48 @@
-import express from 'express';
-import cors from 'cors';
-import { Configuration, OpenAIApi } from 'openai';
-import readlineSync from 'readline-sync';
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+dotenv.config()
 
-// Ask the user for the OpenAI API key
-const apiKey = askUserForApiKey();
-
-let openai;
-if (apiKey) {
-  const configuration = new Configuration({
-    apiKey,
-  });
-  openai = new OpenAIApi(configuration);
-}
-
-const constantResponses = [
-  "How can I assist you?",
-  "How can I help you?",
-  "Is there anything else you'd like to know?",
-  "Feel free to ask any questions.",
-  "I'm here to help. What can I do for you?",
-];
-
-app.get('/', (req, res) => {
-  const randomIndex = Math.floor(Math.random() * constantResponses.length);
-  const defaultMessage = constantResponses[randomIndex];
-
-  res.status(200).send({
-    message: 'Hello from CodeX!',
-    defaultResponse: defaultMessage,
-  });
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
 });
+
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
+})
 
 app.post('/', async (req, res) => {
   try {
-    if (!openai) {
-      const randomIndex = Math.floor(Math.random() * constantResponses.length);
-      const defaultMessage = constantResponses[randomIndex];
-      return res.status(200).send({
-        bot: defaultMessage,
-      });
-    }
-
     const prompt = req.body.prompt;
 
     const response = await openai.createCompletion({
-      model: 'text-davinci-003',
+      model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0,
-      max_tokens: 3000,
-      top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0,
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
     res.status(200).send({
-      bot: response.data.choices[0].text,
+      bot: response.data.choices[0].text
     });
+
   } catch (error) {
-    console.error(error);
+    console.error(error)
     res.status(500).send(error || 'Something went wrong');
   }
-});
+})
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
-
-function askUserForApiKey() {
-  const apiKey = readlineSync.question('Enter your OpenAI API key: ');
-  return apiKey || null; // return null if the user didn't provide the API key
-}
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
