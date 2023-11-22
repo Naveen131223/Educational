90e823fb1040e871e56a8b7cdabb2956f684b334
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-11-22 21:41:19 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,61 +1,154 @@
-// server.js
 import express from 'express';
-import puppeteer from 'puppeteer';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(cors());
+app.use(express.json({ limit: '1mb' }));
+
+const apiKey = process.env.OPENAI_API_KEY;
+
+if (!apiKey) {
+  console.error('Please provide an OPENAI_API_KEY in your environment variables.');
+  process.exit(1);
+}
+
+const configuration = new Configuration({
+  apiKey,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// Simple in-memory cache to store API responses
+const responseCache = {};
+let isAIModelReady = false; // Flag to check if the AI model is ready
+const WARM_UP_PROMPT = 'Warm-up prompt';
+
+// Initialize the AI model asynchronously during server startup
+const modelInitializationPromise = initializeAIModel();
+
+async function initializeAIModel() {
+  try {
+    console.log('Initializing AI model...');
+    const response = await openai.createCompletion({
+      model: process.env.OPENAI_MODEL || 'text-ada-001',
+      prompt: WARM_UP_PROMPT,
+    });
+
+    const botResponse = response.data.choices[0]?.text || 'No response from the AI model.';
+    responseCache[WARM_UP_PROMPT] = botResponse;
+
+    console.log('AI model is ready!');
+    isAIModelReady = true;
+  } catch (error) {
+    console.error('Error initializing AI model:', error);
+  }
+}
+
+// Middleware to check if the AI model is ready before processing requests
+app.use((req, res, next) => {
+  if (!isAIModelReady) {
+    return res.status(200).send({
+      message: 'Initializing AI model, please wait...',
+    });
+  }
+  next();
+});
+
+app.get('/status', (req, res) => {
+  if (isAIModelReady) {
+    return res.status(200).send({
+      status: 'AI model is ready!',
+    });
+  }
+  res.status(200).send({
+    status: 'AI model is initializing...',
+  });
+});
+
+app.get('/', (req, res) => {
+  // If the AI model is ready, return the cached warm-up response immediately
+  return res.status(200).send({
+    bot: responseCache[WARM_UP_PROMPT],
+  });
+});
 
 app.post('/', async (req, res) => {
   try {
-    const { prompt } = req.body;
+    let { prompt } = req.body;
 
     if (!prompt || typeof prompt !== 'string' || prompt.trim() === '') {
       return res.status(400).send({ error: 'Invalid or missing prompt in the request body.' });
     }
 
-    // Launch a headless browser with puppeteer
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    // Sanitize and escape the input prompt to prevent XSS attacks
+    prompt = sanitizeInput(prompt);
 
-    // Perform a search on Google
-    const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(prompt)}`;
-    await page.goto(searchUrl);
+    if (responseCache[prompt]) {
+      console.log('Cache hit for prompt:', prompt);
+      return res.status(200).send({ bot: responseCache[prompt] });
+    }
 
-    // Extract all text content from the search results
-    const results = await page.evaluate(() => {
-      const extractedResults = [];
-      document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, div, span, a, li, td, span, strong, em, b, i, u, s').forEach((result) => {
-        const text = result.textContent;
-        const link = result.tagName === 'A' ? result.href : '';
-        extractedResults.push({ text, link });
-      });
-      return extractedResults;
+    const response = await openai.createCompletion({
+      model: process.env.OPENAI_MODEL || 'text-davinci-003',
+      prompt: `${prompt}`,
+      temperature: 0.2,
+      max_tokens: 500,
+      top_p: 0.5,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
     });
 
-    // Close the browser
-    await browser.close();
+    const botResponse = response.data.choices[0]?.text || 'No response from the AI model.';
+    responseCache[prompt] = botResponse;
 
-    // Respond to the user
-    res.status(200).send({
-      bot: `Search results for "${prompt}" on Google: ${results.map(result => `${result.text} - ${result.link}`).join(', ')}`,
-    });
+    res.status(200).send({ bot: botResponse });
   } catch (error) {
     console.error(error);
     res.status(500).send('Something went wrong');
   }
 });
 
-const server = app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
+// Error handler middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Something went wrong');
 });
 
-// Graceful shutdown
-process.on('SIGTERM', () => {
-  console.log('Shutting down gracefully...');
-  server.close(() => {
-    console.log('Server has been closed.');
-    process.exit(0);
+// Start the server after the AI model is initialized
+modelInitializationPromise.then(() => {
+  const server = app.listen(port, () => {
+    console.log(`AI server started on http://localhost:${port}`);
+  });
+
+  process.on('SIGTERM', () => {
+    console.log('Shutting down gracefully...');
+    server.close(() => {
+      console.log('Server has been closed.');
+      process.exit(0);
+    });
   });
 });
+
+function sanitizeInput(input) {
+  return input.replace(/[&<>"'\/]/g, (char) => {
+    switch (char) {
+      case '&':
+        return '&amp;';
+      case '<':
+        return '&lt;';
+      case '>':
+        return '&gt;';
+      case '"':
+        return '&quot;';
+      case "'":
+        return '&#39;';
+      case '/':
+        return '&#x2F;';
+      default:
+        return char;
+    }
+  });
+}
