62e252a06c82be5b7087c110bbfc11e04424b7e0
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-11-23 11:52:40 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { GPT, GPTResponse } from 'openai';
+import { Configuration, OpenAIApi } from 'openai';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,33 +8,70 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
 
-const gpt = new GPT({ key: process.env.OPENAI_API_KEY });
+const apiKey = process.env.OPENAI_API_KEY;
 
-let isServerReady = true;
+if (!apiKey) {
+  console.error('Please provide an OPENAI_API_KEY in your environment variables.');
+  process.exit(1);
+}
 
+const configuration = new Configuration({
+  apiKey,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// Simple in-memory cache to store API responses
+const responseCache = {};
+let isAIModelReady = false; // Flag to check if the AI model is ready
+const WARM_UP_PROMPT = 'Warm-up prompt';
+
+// Initialize the AI model asynchronously during server startup
+const modelInitializationPromise = initializeAIModel();
+
+async function initializeAIModel() {
+  try {
+    console.log('Initializing AI model...');
+    const response = await openai.createCompletion({
+      model: process.env.OPENAI_MODEL || 'text-ada-001',
+      prompt: WARM_UP_PROMPT,
+    });
+
+    const botResponse = response.data.choices[0]?.text || 'No response from the AI model.';
+    responseCache[WARM_UP_PROMPT] = botResponse;
+
+    console.log('AI model is ready!');
+    isAIModelReady = true;
+  } catch (error) {
+    console.error('Error initializing AI model:', error);
+  }
+}
+
+// Middleware to check if the AI model is ready before processing requests
 app.use((req, res, next) => {
-  if (!isServerReady) {
+  if (!isAIModelReady) {
     return res.status(200).send({
-      message: 'Server is initializing, please wait...',
+      message: 'Initializing AI model, please wait...',
     });
   }
   next();
 });
 
 app.get('/status', (req, res) => {
-  if (isServerReady) {
+  if (isAIModelReady) {
     return res.status(200).send({
-      status: 'Server is ready!',
+      status: 'AI model is ready!',
     });
   }
   res.status(200).send({
-    status: 'Server is initializing...',
+    status: 'AI model is initializing...',
   });
 });
 
 app.get('/', (req, res) => {
+  // If the AI model is ready, return the cached warm-up response immediately
   return res.status(200).send({
-    bot: 'Placeholder response. Replace this with your desired logic.',
+    bot: responseCache[WARM_UP_PROMPT],
   });
 });
 
@@ -46,7 +83,26 @@ app.post('/', async (req, res) => {
       return res.status(400).send({ error: 'Invalid or missing prompt in the request body.' });
     }
 
-    const botResponse = await generateResponse(prompt);
+    // Sanitize and escape the input prompt to prevent XSS attacks
+    prompt = sanitizeInput(prompt);
+
+    if (responseCache[prompt]) {
+      console.log('Cache hit for prompt:', prompt);
+      return res.status(200).send({ bot: responseCache[prompt] });
+    }
+
+    const response = await openai.createCompletion({
+      model: process.env.OPENAI_MODEL || 'text-davinci-003',
+      prompt: `${prompt}`,
+      temperature: 0.2,
+      max_tokens: 500,
+      top_p: 0.5,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    });
+
+    const botResponse = response.data.choices[0]?.text || 'No response from the AI model.';
+    responseCache[prompt] = botResponse;
 
     res.status(200).send({ bot: botResponse });
   } catch (error) {
@@ -55,30 +111,44 @@ app.post('/', async (req, res) => {
   }
 });
 
+// Error handler middleware
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send('Something went wrong');
 });
 
-const server = app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
-  isServerReady = true;
-});
+// Start the server after the AI model is initialized
+modelInitializationPromise.then(() => {
+  const server = app.listen(port, () => {
+    console.log(`AI server started on http://localhost:${port}`);
+  });
 
-process.on('SIGTERM', () => {
-  console.log('Shutting down gracefully...');
-  server.close(() => {
-    console.log('Server has been closed.');
-    process.exit(0);
+  process.on('SIGTERM', () => {
+    console.log('Shutting down gracefully...');
+    server.close(() => {
+      console.log('Server has been closed.');
+      process.exit(0);
+    });
   });
 });
 
-async function generateResponse(prompt): Promise<string> {
-  // Example using GPT-2.5-turbo
-  const response: GPTResponse = await gpt.complete({
-    prompt: prompt,
-    model: 'gpt-2.5-turbo',
+function sanitizeInput(input) {
+  return input.replace(/[&<>"'\/]/g, (char) => {
+    switch (char) {
+      case '&':
+        return '&amp;';
+      case '<':
+        return '&lt;';
+      case '>':
+        return '&gt;';
+      case '"':
+        return '&quot;';
+      case "'":
+        return '&#39;';
+      case '/':
+        return '&#x2F;';
+      default:
+        return char;
+    }
   });
-
-  return response.choices[0]?.text || 'No response from the language model.';
-}
+ }
