aa8744fbe4d1511fb9f0abc9cf32f2be0480f8b3
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-11-23 16:26:43 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import axios from 'axios';
+import { Configuration, OpenAIApi } from 'openai';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,11 +8,46 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
 
-const PLAYGROUND_URL = 'https://play.openai.com';
+const apiKey = process.env.OPENAI_API_KEY;
+
+if (!apiKey) {
+  console.error('Please provide an OPENAI_API_KEY in your environment variables.');
+  process.exit(1);
+}
+
+const configuration = new Configuration({
+  apiKey,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// Simple in-memory cache to store API responses
+const responseCache = {};
+let isAIModelReady = false; // Flag to check if the AI model is ready
+const WARM_UP_PROMPT = 'Warm-up prompt';
+
+// Initialize the AI model asynchronously during server startup
+const modelInitializationPromise = initializeAIModel();
+
+async function initializeAIModel() {
+  try {
+    console.log('Initializing AI model...');
+    const response = await openai.createCompletion({
+      model: process.env.OPENAI_MODEL || 'text-ada-001',
+      prompt: WARM_UP_PROMPT,
+    });
+
+    const botResponse = response.data.choices[0]?.text || 'No response from the AI model.';
+    responseCache[WARM_UP_PROMPT] = botResponse;
+
+    console.log('AI model is ready!');
+    isAIModelReady = true;
+  } catch (error) {
+    console.error('Error initializing AI model:', error);
+  }
+}
 
 // Middleware to check if the AI model is ready before processing requests
-let isAIModelReady = false;
-
 app.use((req, res, next) => {
   if (!isAIModelReady) {
     return res.status(200).send({
@@ -34,8 +69,10 @@ app.get('/status', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  // If the AI model is ready, return the response from the Playground
-  return res.redirect(PLAYGROUND_URL);
+  // If the AI model is ready, return the cached warm-up response immediately
+  return res.status(200).send({
+    bot: responseCache[WARM_UP_PROMPT],
+  });
 });
 
 app.post('/', async (req, res) => {
@@ -46,12 +83,27 @@ app.post('/', async (req, res) => {
       return res.status(400).send({ error: 'Invalid or missing prompt in the request body.' });
     }
 
-    // Send the prompt to the Playground API
-    const response = await axios.post(PLAYGROUND_URL + '/api/chat/completions', {
-      messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: prompt }],
+    // Sanitize and escape the input prompt to prevent XSS attacks
+    prompt = sanitizeInput(prompt);
+
+    if (responseCache[prompt]) {
+      console.log('Cache hit for prompt:', prompt);
+      return res.status(200).send({ bot: responseCache[prompt] });
+    }
+
+    const response = await openai.createCompletion({
+      model: process.env.OPENAI_MODEL || 'text-davinci-003',
+      prompt: `${prompt}`,
+      temperature: 0.2,
+      max_tokens: 500,
+      top_p: 0.5,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
     });
 
-    const botResponse = response.data.choices[0]?.message?.content || 'No response from the AI model.';
+    const botResponse = response.data.choices[0]?.text || 'No response from the AI model.';
+    responseCache[prompt] = botResponse;
+
     res.status(200).send({ bot: botResponse });
   } catch (error) {
     console.error(error);
@@ -65,17 +117,38 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something went wrong');
 });
 
-// Start the server
-const server = app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
-  isAIModelReady = true;
-});
+// Start the server after the AI model is initialized
+modelInitializationPromise.then(() => {
+  const server = app.listen(port, () => {
+    console.log(`AI server started on http://localhost:${port}`);
+  });
 
-// Graceful shutdown
-process.on('SIGTERM', () => {
-  console.log('Shutting down gracefully...');
-  server.close(() => {
-    console.log('Server has been closed.');
-    process.exit(0);
+  process.on('SIGTERM', () => {
+    console.log('Shutting down gracefully...');
+    server.close(() => {
+      console.log('Server has been closed.');
+      process.exit(0);
+    });
   });
 });
+
+function sanitizeInput(input) {
+  return input.replace(/[&<>"'\/]/g, (char) => {
+    switch (char) {
+      case '&':
+        return '&amp;';
+      case '<':
+        return '&lt;';
+      case '>':
+        return '&gt;';
+      case '"':
+        return '&quot;';
+      case "'":
+        return '&#39;';
+      case '/':
+        return '&#x2F;';
+      default:
+        return char;
+    }
+  });
+ }
