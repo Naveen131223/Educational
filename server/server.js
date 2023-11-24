2d98400986eae57ebb0c30e5e076d1adc0f6c282
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2023-11-24 22:04:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,21 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Simple in-memory cache
-const responseCache = {};
-
-// Load the GPT-2 model initially
-let gptModel;
-
-(async () => {
-  try {
-    gptModel = await gpt2.loadModel({ fromCache: true });
-    console.log('GPT-2 model loaded.');
-  } catch (error) {
-    console.error('Error loading GPT-2 model:', error);
-  }
-})();
-
 app.get('/', (req, res) => {
   res.status(200).send('Server is up and running!');
 });
@@ -35,17 +20,8 @@ app.post('/getResponse', async (req, res) => {
       return res.status(400).send({ error: 'Invalid or missing prompt in the request body.' });
     }
 
-    // Check if the response is cached
-    if (responseCache[prompt]) {
-      console.log('Cache hit for prompt:', prompt);
-      return res.status(200).send({ bot: responseCache[prompt] });
-    }
-
-    // If not cached, generate response using GPT-2
+    // Generate response using dynamically loaded GPT-2 model
     const botResponse = await generateResponse(prompt);
-
-    // Cache the response
-    responseCache[prompt] = botResponse;
 
     res.status(200).send({ bot: botResponse });
   } catch (error) {
@@ -58,17 +34,6 @@ const server = app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-// Periodically load the GPT-2 model into memory (every 24 hours in this example)
-setInterval(async () => {
-  console.log('Reloading GPT-2 model into memory...');
-  try {
-    gptModel = await gpt2.loadModel({ fromCache: true });
-    console.log('GPT-2 model reloaded.');
-  } catch (error) {
-    console.error('Error reloading GPT-2 model:', error);
-  }
-}, 24 * 60 * 60 * 1000); // Reload every 24 hours
-
 process.on('SIGTERM', () => {
   console.log('Shutting down gracefully...');
   server.close(() => {
@@ -79,8 +44,14 @@ process.on('SIGTERM', () => {
 
 async function generateResponse(prompt) {
   try {
-    // Generate response using the loaded GPT-2 model
-    const botResponse = await gpt2(gptModel, prompt);
+    // Load the GPT-2 model dynamically from the online source
+    const model = await gpt2.loadModel({ fromCache: false });
+
+    // Generate response using the loaded model
+    const botResponse = await gpt2(model, prompt);
+
+    // Unload the model to free up memory (optional)
+    await gpt2.unloadModel(model);
 
     return botResponse;
   } catch (error) {
