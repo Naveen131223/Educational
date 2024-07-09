68cb5f8192ad96471624526c97efc9390b49400f
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2024-07-09 10:00:15 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const clearCache = () => {
 };
 
 // Set intervals
-const cacheClearInterval = 1 * 60 * 1000; // 1 minutes in milliseconds
+const cacheClearInterval = 10 * 60 * 1000; // 10 minutes in milliseconds
 setInterval(clearCache, cacheClearInterval);
 
 const sanitizeResponse = (response) => {
