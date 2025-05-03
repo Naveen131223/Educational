f152e855d2a6ccb77b4b0c9096fdc4b5d527510c
Naveen131223 <138207288+Naveen131223@users.noreply.github.com>
2025-05-03 10:44:55 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 dotenv.config();
 
-const HF_API_URL = 'https://api-inference.huggingface.co/models/google/flan-t5-large';
+const HF_API_URL = 'https://api-inference.huggingface.co/models/ahxt/LiteLlama-460M-1T';
 const HF_API_KEY = process.env.HF_API_KEY;
 
 const app = express();
